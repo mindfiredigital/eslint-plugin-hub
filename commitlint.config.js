0862427652a6f0a6227f3693e0c4b592496834fb
anandmindfire <anand.k@mindfiresolutions.com>
2024-09-06 17:55:51 +0530

    fix: fix commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
-const nlp = require('compromise');
-
-function isImperative(sentence) {
-  const doc = nlp(sentence);
-  return doc.verbs().isImperative().out('array').length > 0;
-}
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [message => message.includes('[skip-commitlint]')],
@@ -19,25 +12,5 @@ module.exports = {
     'subject-full-stop': [2, 'never', '.'],
     'subject-max-length': [2, 'always', 100],
     'subject-case': [0, 'never'],
-    'custom-commit-format': [2, 'always'],
   },
-  plugins: [
-    {
-      rules: {
-        'custom-commit-format': parsed => {
-          const { subject } = parsed;
-
-          // Check if subject is in present imperative tense
-          if (!isImperative(subject)) {
-            return [
-              false,
-              'Subject should be in present imperative tense (e.g., "add feature", "fix bug")',
-            ];
-          }
-
-          return [true];
-        },
-      },
-    },
-  ],
 };
