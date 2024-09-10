9807b17f769f8d7331955f64a44e88c4b5a07a4c
anandmindfire <anand.k@mindfiresolutions.com>
2024-09-10 13:57:20 +0530

    fix: fix rule for lowercase regex

--- test/filename-lowercase.test.js
+++ test/filename-lowercase.test.js
@@ -8,46 +8,44 @@ ruleTester.run('filename-lowercase', rules['filename-lowercase'], {
   valid: [
     {
       filename: path.join(__dirname, 'validfile.js'),
-      code: 'console.log("Valid lowercase file name");',
+      code: 'console.log("Valid lowercase filename");',
     },
     {
       filename: path.join(__dirname, 'short.js'),
-      code: 'console.log("Short lowercase file name");',
+      code: 'console.log("Valid lowercase filename");',
     },
     {
-      filename: path.join(__dirname, 'folder', 'validfile.js'),
-      code: 'console.log("Valid lowercase file name in folder");',
+      filename: path.join(__dirname, 'some-dir', 'validfile.js'),
+      code: 'console.log("Valid lowercase filename");',
     },
     {
-      filename: path.join(__dirname, 'folder', 'short.js'),
-      code: 'console.log("Short lowercase file name in folder");',
+      filename: path.join(__dirname, 'valid-file-name.js'),
+      code: 'console.log("Valid lowercase filename with hyphens");',
     },
   ],
 
   invalid: [
     {
       filename: path.join(__dirname, 'InvalidFileName.js'),
-      code: 'console.log("Invalid file name");',
+      code: 'console.log("Invalid filename");',
       errors: [
         { messageId: 'invalidFileName', data: { name: 'InvalidFileName.js' } },
       ],
     },
     {
-      filename: path.join(__dirname, 'Invalid.js'),
-      code: 'console.log("Invalid file name");',
-      errors: [{ messageId: 'invalidFileName', data: { name: 'Invalid.js' } }],
+      filename: path.join(__dirname, 'INVALID.js'),
+      code: 'console.log("Invalid filename");',
+      errors: [{ messageId: 'invalidFileName', data: { name: 'INVALID.js' } }],
     },
     {
-      filename: path.join(__dirname, 'Folder', 'InvalidFileName.js'),
-      code: 'console.log("Invalid file name in folder");',
+      filename: path.join(__dirname, 'Invalid-File-Name.js'),
+      code: 'console.log("Invalid filename");',
       errors: [
-        { messageId: 'invalidFileName', data: { name: 'InvalidFileName.js' } },
+        {
+          messageId: 'invalidFileName',
+          data: { name: 'Invalid-File-Name.js' },
+        },
       ],
-    },
-    {
-      filename: path.join(__dirname, 'Folder', 'Invalid.js'),
-      code: 'console.log("Invalid file name in folder");',
-      errors: [{ messageId: 'invalidFileName', data: { name: 'Invalid.js' } }],
     },
   ],
 });
