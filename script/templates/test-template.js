5f93742352b2f7e1f1a279bf737412f1e3a39f6f
anandmindfire <anand.k@mindfiresolutions.com>
2024-09-20 22:35:36 +0530

    fix: fix general rules tests template

--- script/templates/test-template.js
+++ script/templates/test-template.js
@@ -1,6 +1,6 @@
 const testTemplate = ruleName => `
 const { RuleTester } = require('eslint');
-const rules = require('../../index').pluginHub.rules;
+const rules = require('../../index').rules;
 
 const ruleTester = new RuleTester();
 
